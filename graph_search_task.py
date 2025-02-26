b30a70dbcdae18fef5419b720221d4abcb5e187b
Miguel Jarama <litoguemi_lw@hotmail.com>
2025-02-26 19:07:06 +0100

    Data loading and visualization of workpiece graph

--- graph_search_task.py
+++ graph_search_task.py
@@ -1,10 +1,32 @@
+import networkx as nx
+import json
+
 # ##################################################
 # 1) Load workpiece graph and feature graph data from  json file
 # ##################################################
 
 # Note: Available files are: workpiece_graph.json, feature_graph.json
 
-# TODO
+def load_graph_from_json(file_path):
+    with open(file_path, 'r') as f:
+        data = json.load(f)
+    
+    G = nx.Graph()
+
+    #Adding nodes
+    for node in data['nodes']:
+        node_id = node[0]
+        node_attributes = node[1]
+        G.add_node(node_id, **node_attributes)
+
+    #Adding edges
+    for edge in data['edges']:
+        source = edge[0]
+        target = edge[1]
+        edge_attributes = edge[2]
+        G.add_edge(source, target, **edge_attributes)
+    
+    return G 
 
 # ##################################################
 # 2) Create graphs from loaded data
@@ -13,14 +35,16 @@
 # Hint: The library networkx helps you to create a graph. You can use the nx.Graph() class to create a graph.
 # Note: Other appraoches are also possible.
 
-# TODO
+workpiece_graph = load_graph_from_json('workpiece_graph.json')
+feature_graph = load_graph_from_json('feature_graph.json')
 
 # Note: Optional task - Visualize the graph
 # Example code:
-# from pyvis.network import Network
-# nt = Network()
-# nt.from_nx(workpiece_graph)
-# nt.show("graph.html", notebook=False)
+
+from pyvis.network import Network
+nt = Network()
+nt.from_nx(workpiece_graph)
+nt.show("graph.html", notebook=False)
 
 # ##################################################
 # 3) Check if the feature graph is a subgraph of the workpiece workpiece and find any other matching subgraphs
